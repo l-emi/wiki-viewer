024d38f94faa8c602a787b259ed9d6948b4191b0
Lidiya <lemi.dev@gmail.com>
2016-10-03 11:28:25 +0100

    Tidy HTML and CSS, comment out JS

--- wikiv.js
+++ wikiv.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     
+    //on pressing the enter key
     $(document).keypress(function(e) {
         if(e.which == 13) { 
            
+           //take the search term the user entered
            var searchTerm = $('#searchy').val();
+           //use it to make an ajax call
            var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
             
             $.ajax({
@@ -13,7 +16,8 @@ $(document).ready(function () {
                 async: false,
                 dataType: "json",
                 success: function(data){
-                    $('#output').html('');
+                    //display the results
+                    $('#output').html("");
                 for (var i = 0; i < data[1].length; i++) {
                     $('#output').prepend("<li><a href=" + data[3][i] + "><h4>" +  data[1][i] + "</h4><p>" + data[2][i] + "</p></a></li>");
                     
@@ -21,7 +25,7 @@ $(document).ready(function () {
                     $('#resultati').css("top", "16%");
                 }
                     
-                    $('#searchy').val('');
+                    $('#searchy').val("");
             },
                 error: function(errorMessage){
                 alert("Error!");
@@ -32,12 +36,16 @@ $(document).ready(function () {
     });
     
     
-    
-    $('.port').click(function() {
+    /****************
+    * Outgoing links
+    *****************/
+    //portfolio
+    $(".port").click(function() {
         window.open("http://l-emi.github.io/");
     });
     
-    $('#randy').click(function() {
+    //click Random to be taken to a random Wikipedia article
+    $("#randy").click(function() {
         window.open("https://en.wikipedia.org/wiki/Special:Random");
     });
     
